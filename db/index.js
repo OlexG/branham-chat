e34520a7bd816f60cedfa96ea30efa50ea2a1e22
agorpynich <oleksandr.gorpynich@gmail.com>
2022-03-04 09:45:43 -0800

    Save messages

--- db/index.js
+++ db/index.js
@@ -1,2 +1,51 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./data/messages.db');
+
+class DBManager {
+  async initialize() {
+    const res = new Promise((resolve, reject) => {
+      this.db = new sqlite3.Database('./data/messages.db', err => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+    await res;
+    this.db.run(
+      `CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        msg TEXT,
+        date TEXT,
+      )`
+    );
+  }
+  writeMessage(msg, date) {
+    return new Promise((resolve, reject) => {
+      this.db.run(
+        `INSERT INTO messages (msg, timestamp) VALUES (?, ?)`,
+        [msg, date],
+        err => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        }
+      );
+    });
+  }
+  getMessages() {
+    return new Promise((resolve, reject) => {
+      this.db.all(`SELECT * FROM messages`, (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+  }
+}
+
+module.exports = DBManager;
--- index.js
+++ index.js
@@ -7,6 +7,12 @@ require('dotenv').config();
 const app = express();
 const server = http.createServer(app);
 const io = new socketio.Server(server);
+const DBManager = require('./db');
+
+(async () => {
+
+
+await DBManager.initialize();
 
 io.on('connection', (socket) => {
 	socket.on('chat message', ({ msg, metadata }) => {
@@ -17,10 +23,20 @@ io.on('connection', (socket) => {
 		}
 		let package = { msg, metadata: newMetadata };
 		io.emit('chat message', package);
+    DBManager.writeMessage(msg, dateNow);
 	});
+
+  socket.on('get chat messages', () => {
+    DBManager.getMessages().then(rows => {
+      socket.emit('chat messages', rows);
+    });
+  })
 });
 app.use(express.static("public"));
 
 server.listen(process.env.PORT ?? 3000, () => {
   console.log(`Listening on port ${process.env.PORT}`);
 });
+
+
+})();
