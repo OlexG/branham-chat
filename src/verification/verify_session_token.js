460872f94a6e2d75befa411574cfed2f0d042388
agorpynich <oleksandr.gorpynich@gmail.com>
2022-03-21 19:15:52 -0700

    No auth header handling

--- src/verification/verify_session_token.js
+++ src/verification/verify_session_token.js
@@ -2,6 +2,7 @@ import DBManager from "../db.js";
 
 const db_manager = new DBManager();
 export default async function verify_session_token(req, res, next) {
+  if (!req.headers.authorization) return res.status(401).end("No token");
   const token = req.headers.authorization.split(' ')[1];
   const email = req.headers.email;
   if (!token) {
